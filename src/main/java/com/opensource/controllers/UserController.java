c858817cab2ebb6d9656b7b4942dec12b487f2df
xXstrawcakeXx <107396089+xXstrawcakeXx@users.noreply.github.com>
2022-11-28 14:56:46 -0800

    new user controller methods and initial sql db values

--- src/main/java/com/opensource/controllers/UserController.java
+++ src/main/java/com/opensource/controllers/UserController.java
@@ -1,18 +1,46 @@
 package com.opensource.controllers;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.opensource.models.User;
+import com.opensource.services.UserService;
+
 @RestController
 @RequestMapping("/users")
-@CrossOrigin (origins= {"http://localhost:8080"})
+@CrossOrigin(origins = { "http://localhost:8080" })
 public class UserController {
+
+	UserService uServ;
 	
-//	UserService uServ;
-//	
-//	public UserController (UserService uServ) {
-//		this.uServ = uServ;
-//	}
+	public UserController (UserService uServ) {
+		this.uServ = uServ;
+	}
+
+	@GetMapping("/{userid}")
+	public ResponseEntity<Optional<User>> getUserById(@RequestHeader("userid") int userId) {
+		return ResponseEntity.ok(uServ.findById(userId));
+	}
+	
+	@GetMapping("/{username}")
+	public ResponseEntity<Optional<User>> getUserByUsername(@RequestHeader("username") String username) {
+		return ResponseEntity.ok(uServ.findByUsername(username));
+	}
+	
+	@GetMapping("/{email}")
+	public ResponseEntity<Optional<User>> getUserByEmail(@RequestHeader("email") String userEmail) {
+		return ResponseEntity.ok(uServ.findByEmail(userEmail));
+	}
+	
+	
+	
+	
+	
 	
 }
---
+++ src/main/resources/social-bee.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (
+firstname, lastname, username, password, email, profile_pic) 
+VALUES (
+"firstnameTest1", "lastnameTest1", "usernameTest1", "passwordTest1", "email@gmail.com", "profilePicTest1")
+"Frodo", "Baggins", "theRealFrodo", "loserClub123", "frodo_baggins@gmail.com", "profilePicTest1")
+"Samwise", "Gamgee", "sammy", "gardenHive", "samwiseG@gmail.com", "Hi_There")
+);
